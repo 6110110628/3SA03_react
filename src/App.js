909e0f51ba9d7e6ba87ae845605d64d68c9b923b
6110110628 <6110110628@psu.ac.th>
2020-08-13 20:48:14 +0700

    part4

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import './App.css';
-import CharacterCard from './CharacterCard';
+//import WordCard from './WordCard';
 
 const word = "Hello";
 function App() {
- return (
-  <div>
+  return (
+    <div>
     {
-      Array.from(word).map((c, i) => <CharacterCard value={c} key={i}/>)
+        Array.from(word).map((c, i) => <CharacterCard value={c} key={i}/>)
     }
-  </div>
- );
+    </div>
+  );
+
 }
 
 export default App;
